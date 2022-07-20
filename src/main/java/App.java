b6684019ad788e7f9fc2b14942bf09b77ce02976
Ivan Zernov <24sneakers1@gmail.com>
2022-07-20 22:40:14 +0300

    123

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloBean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloBean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloBean == helloBean2);
+        System.out.println(catBean == catBean2);
     }
 }
